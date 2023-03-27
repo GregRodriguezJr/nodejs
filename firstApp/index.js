06c97fb83f4054dda8f771518964b220078abc1b
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-27 18:43:08 -0500

    test express route basics

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    console.log("Requested");
-    res.send("Hi, we got your resquest. This is your response!")
+// app.use((req, res) => {
+//     console.log("Requested");
+//     res.send("Hi, we got your resquest. This is your response!")
+// });
+
+// Test get request
+app.get("/", (req, res) => {
+    res.send("Home/Root!");
+});
+
+app.get("/products", (req, res) => {
+    res.send("Products!");
+});
+
+app.get("/details", (req, res) => {
+    res.send("Details!");
+});
+
+app.get("*", (req, res) => {
+    res.send("Path not valid");
 });
 
 app.listen(3000, () => {
