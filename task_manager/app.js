a1c1dc627ac9b1464fb65932f9d1e3e69293bed7
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-28 11:42:22 -0500

    created route for getAllTasks

--- task_manager/app.js
+++ task_manager/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+
+// Middleware
+app.use(express.json());
 
 // Routes
 app.get("/hello", (req, res) => {
     res.send("Task Manager App");
 });
 
-
+app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
