64b348a435b1fe9fd63058490f6bd861564e224e
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-05-23 15:45:29 -0500

    refactored db connect

--- task_manager/db/connect.js
+++ task_manager/db/connect.js
@@ -1,2 +1,15 @@
-const connectionString = 
-    "mongodb+srv://84grod:<password>@nodeexpressprojects.w2xux5q.mongodb.net/?retryWrites=true&w=majority"
+const mongoose = require("mongoose");
+
+const connectionString =
+    "mongodb+srv://84grod:<password>@nodeexpressprojects.w2xux5q.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
+
+const connectDB = () => {
+    return mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    });
+};
+
+module.exports = connectDB;
