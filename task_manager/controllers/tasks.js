856dd27d28390904af13f7db4a5b3284de1b7d35
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-05-26 20:06:00 -0500

    completed updateTask, added return statements to CRUD functions

--- task_manager/controllers/tasks.js
+++ task_manager/controllers/tasks.js
@@ -5,19 +5,18 @@ const getAllTasks = async (req, res) => {
         // find all documents
         // await MyModel.find({});
         const task = await Task.find({});
-        res.status(200).json({ task });
+        return res.status(200).json({ task });
     } catch (error) {
-        res.status(500).json({ msg: error });
+        return res.status(500).json({ msg: error });
     }
-    res.send("get all items");
 };
 
 const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body);
-        res.status(201).json({ task });
+        return res.status(201).json({ task });
     } catch (error) {
-        res.status(500).json({ msg: error });
+        return res.status(500).json({ msg: error });
     }
 };
 
@@ -28,14 +27,33 @@ const getTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({ msg: `No task with Id: ${taskID}` });
         }
-        res.status(200).json({ task });
+        return res.status(200).json({ task });
     } catch (error) {
-        res.status(500).json({ msg: error });
+        return res.status(500).json({ msg: error });
     }
 };
 
-const updateTask = (req, res) => {
-    res.send("update task");
+const updateTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        if (!task) {
+            return res
+                .status(404)
+                .json({ msg: `No task with the Id: ${taskID}` });
+        }
+
+        return res.status(200).json({ task });
+
+        // log to test in postman
+        // res.status(200).json({ id: taskID, data: req.body });
+    } catch (error) {
+        return res.status(500).json({ msg: error });
+    }
 };
 
 const deleteTask = async (req, res) => {
@@ -45,9 +63,9 @@ const deleteTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({ msg: `No task with Id: ${taskID}` });
         }
-        res.status(200).json({ task });
+        return res.status(200);
     } catch (error) {
-        res.status(500).json({ msg: error });
+        return res.status(500).json({ msg: error });
     }
 };
 
